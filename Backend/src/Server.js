68ac70994229015dda0713a2b458f183483d48ff
govind-anjana <govindanjana2004@gmail.com>
2025-10-09 23:14:45 +0530

    Edit Files

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import ConnectionData from "./config/db.js";
+import ConnectionData from "../src/config/Db.js";
 import app from './app.js'
 const PORT = process.env.PORT || 8000;
 ConnectionData();
