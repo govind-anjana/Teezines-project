2d079b01e4fe71a219e0d771060956898e986e41
govind-anjana <govindanjana2004@gmail.com>
2025-10-24 15:00:58 +0530

    edit category routes are change the middleware for verifyadmin

--- Backend/src/routes/CategoryRoutes.js
+++ Backend/src/routes/CategoryRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import { CategoryAdd, CategoryDelete, CategoryUpdate, Getcategory } from '../controller/CategoryController.js';
+import { verifyAdmin } from '../middleware/auth.js';
 
 const router=express.Router();
 
-router.get("/",Getcategory);
+router.get("/",verifyAdmin,Getcategory);
 
-router.post("/",CategoryAdd);
+router.post("/",verifyAdmin,CategoryAdd);
 
-router.put("/:id",CategoryUpdate);
+router.put("/:id",verifyAdmin,CategoryUpdate);
 
-router.delete("/:id",CategoryDelete)
+router.delete("/:id",verifyAdmin,CategoryDelete)
 
 
 export default router
