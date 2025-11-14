9a9767a95ab82ae5b39e285ee4c890ef7f5970b3
govind-anjana <govindanjana2004@gmail.com>
2025-11-14 17:22:59 +0530

    userModel are edit the new field to googleId,authType

--- Backend/src/model/UserModel.js
+++ Backend/src/model/UserModel.js
@@ -2,18 +2,20 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String },
   email:    { type: String, required: true, unique: true },
   dateOfBirth:{
     type: Date,           // <-- important: store DOB as Date
     required: false       // make true if you want to make it mandatory
   },
-  password: { type: String, required: true },
+  password: { type: String, required:false },
    phone:    { type: String, default: "" },
   address:  { type: String, default: "" },
   isVerified: { type: Boolean, default: false },
   otpHash: { type: String },            
-  otpExpiresAt: { type: Date },         
+  otpExpiresAt: { type: Date },  
+  authType: { type: String, default: "manual" }, 
+  googleId: { type: String },           
  hasUsedAI: { type: Boolean, default: false },
   verificationToken: { type: String },
 }, { timestamps: true });
